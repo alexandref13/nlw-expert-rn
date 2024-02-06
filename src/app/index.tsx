6f857e9c93acbe26fec8696ef2bf5367e7059c29
Alexandre Fernandes <alefernandeseng@gmail.com>
2024-02-06 00:57:16 -0300

    feat: aula 01

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,37 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, FlatList } from 'react-native';
+
+import { CATEGORIES } from '@/utils/data/products';
+import { CategoryButton } from '@/components/category-button';
+import { Header } from '@/components/header';
 
 export default function Home() {
+  const [categoryIndex, setCategoryIndex] = useState(0);
+
+  function handleCategorySelectedIndex({ index }: { index: number }) {
+    setCategoryIndex(index);
+  }
+
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text className="text-white text-2xl">Hello, world!</Text>
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" />
+      <FlatList
+        data={CATEGORIES}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        className="max-h-10 mt-5"
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        keyExtractor={(item) => item}
+        renderItem={({ item, index }) => (
+          <CategoryButton
+            title={item}
+            isSelected={index === categoryIndex}
+            onPress={() => {
+              handleCategorySelectedIndex({ index });
+            }}
+          />
+        )}
+      />
     </View>
   );
 }
